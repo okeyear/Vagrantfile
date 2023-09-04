22f42d68ea0c4c86fb2f4dbea7a2a1b035288fee
Jack Peng <okeyear@163.com>
2023-09-04 15:54:24 +0800

    update recommanded vagrantfile

--- Recommended/add_etc_hosts.sh
+++ Recommended/add_etc_hosts.sh
@@ -11,12 +11,17 @@ export LANG=en
 # delete old content, from line "# add by vagrant begin" to "# add by vagrant end"
 sudo sed -i '/# add by vagrant begin/,/# add by vagrant end/d' /etc/hosts
 
-serverip=$1
-servercount=$2
-nodeip=$3
-nodecount=$4
-clientip=$5
-clientcount=$6
+domain=$1
+serverip=$2
+servercount=$3
+nodeip=$4
+nodecount=$5
+clientip=$6
+clientcount=$7
+
+# domain
+sudo sed -i '/^search/d' /etc/resolv.conf
+echo "search $domain" | sudo tee -a /etc/resolv.conf
 
 # server
 echo '# add by vagrant begin' | sudo tee -a /etc/hosts
@@ -24,7 +29,7 @@ serverip_head=${serverip%.*}
 serverip_tail=${serverip##*.}
 for i in $( seq 0 $(($servercount-1)) )
 do
-    printf "%-16s    server-%02d\n" ${serverip_head}.$((${serverip_tail}+i)) $(($i+1)) | sudo tee -a /etc/hosts
+    printf "%-16s    server-%02d.${domain}    server-%02d\n" ${serverip_head}.$((${serverip_tail}+i)) $(($i+1)) $(($i+1)) | sudo tee -a /etc/hosts
 done
 
 # node
@@ -32,7 +37,7 @@ nodeip_head=${nodeip%.*}
 nodeip_tail=${nodeip##*.}
 for i in $( seq 0 $(($nodecount-1)) )
 do
-    printf "%-16s    node-%02d\n" ${nodeip_head}.$((${nodeip_tail}+i)) $(($i+1)) | sudo tee -a /etc/hosts
+    printf "%-16s    node-%02d.${domain}    node-%02d\n" ${nodeip_head}.$((${nodeip_tail}+i)) $(($i+1)) $(($i+1)) | sudo tee -a /etc/hosts
 done
 
 # client
@@ -40,6 +45,6 @@ clientip_head=${clientip%.*}
 clientip_tail=${clientip##*.}
 for i in $( seq 0 $(($clientcount-1)) )
 do
-    printf "%-16s    client-%02d\n" ${clientip_head}.$((${clientip_tail}+i)) $(($i+1)) | sudo tee -a /etc/hosts
+    printf "%-16s    client-%02d.${domain}    client-%02d\n" ${clientip_head}.$((${clientip_tail}+i)) $(($i+1)) $(($i+1)) | sudo tee -a /etc/hosts
 done
 echo '# add by vagrant end' | sudo tee -a /etc/hosts
